8f910b39fbb98ce0441ec6d493f98b1275c67166
J.J.Ding <louis7012161@gmail.com>
2022-02-13 13:13:30 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region Old Code
+
+            //List<int> text = new List<int>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    text.Add(i);
+            //}
+
+            //List<string> result = new List<string>();
+            //foreach (int item in text)
+            //{
+            //    result.Add(item.ToString());
+            //}
+
+            #endregion Old Code
+
+            List<int> text = Enumerable.Range(0, 10).ToList();
+            List<string> result = text.Select(x => x.ToString()).ToList();
+        }
+    }
+}
